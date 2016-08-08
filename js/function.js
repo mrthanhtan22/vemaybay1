1e535dbd69381d8ef300484bb20664c2e7834c13
Pham Thanh Tan <mrthanhtan22@gmail.com>
2016-08-08 23:29:52 +0700

    8/8/2016

--- js/function.js
+++ js/function.js
@@ -3,13 +3,11 @@ $(document).ready(function(){
 	/* slider gia ve khuyen mai */
   $('.bxslider1').bxSlider({
     mode: 'vertical',
-    minSlides: 3,
+    minSlides: 2,
     slideMargin: 10,
     auto: true
   });
- });
-  /* slider ve noidia*/
-  
+
   	$('.slide2').bxSlider({
     slideWidth: 900,
     minSlides: 3,
@@ -21,4 +19,19 @@ $(document).ready(function(){
    	slideMargin: 10
   });
 
-   
+      $(".navbar a, footer a[href='#myPage']").on('click',function(event) {
+        if (this.hash !== "") {
+          event.preventDefault();
+        /* Act on the event */
+        var hash =this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top,
+          },
+          900, function() {
+          /* stuff to do after animation is complete */
+          window.location.hash = hash;
+        });
+        }
+        
+      });
+   })
