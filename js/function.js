7d526a46cd66498ae7cb64003b05bc04e4a1a5e5
tan <mrthanhtan22@gmail.com>
2016-08-08 17:22:40 +0700

    thay doi

--- js/function.js
+++ js/function.js
@@ -1,9 +1,24 @@
 /*Jquery */
 $(document).ready(function(){
+	/* slider gia ve khuyen mai */
   $('.bxslider1').bxSlider({
     mode: 'vertical',
     minSlides: 3,
     slideMargin: 10,
     auto: true
   });
-});
+ });
+  /* slider ve noidia*/
+  
+  	$('.slide2').bxSlider({
+    slideWidth: 900,
+    minSlides: 3,
+    maxSlides: 3,
+    moveSlides: 3,
+    slideMargin: 0,
+    auto: true,
+   	padding: 10,
+   	slideMargin: 10
+  });
+
+   
