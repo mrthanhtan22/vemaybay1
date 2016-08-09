9b0f5b2ae96dd881d6c24d5171f012f41f2ce14d
tan <mrthanhtan22@gmail.com>
2016-08-09 17:27:36 +0700

    9/8/2016

--- js/function.js
+++ js/function.js
@@ -41,4 +41,15 @@ $(document).ready(function(){
       });
     } // End if
   });
+      //ngay thang nam
+      if (!Modernizr.inputtypes.date) {
+            // If not native HTML5 support, fallback to jQuery datePicker
+            $('input[type=date]').datepicker({
+                // Consistent format with the HTML5 picker
+                    dateFormat : 'dd-mm-yy'
+                },
+                // Localization
+                $.datepicker.regional['it']
+            );
+        }
    })
